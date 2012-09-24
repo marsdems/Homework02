ab13158e85eee827f3962057e3174d4cff7dfe17
Mike Marsden <marsdems@muohio.edu>
2012-09-23 21:34:54 -0400

    Continued Work

--- src/Homework02App.cpp
+++ src/Homework02App.cpp
@@ -2,7 +2,6 @@
 #include "cinder/gl/gl.h"
 #include "Circle.h"
 #include "cinder/app/AppNative.h"
-#include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Text.h"
 #include "cinder/Rand.h"
@@ -27,11 +26,18 @@ class Homework02App : public AppBasic {
 	Font				tempFont;
 	gl::TextureFontRef	mTextureFont;
 private:
-	Circle* circle_list_;
-	static const int kAppWidth=1280;
-	static const int kAppHeight=720;
+	Circle* circleList_;
+	static const int kAppWidth=800;
+	static const int kAppHeight=600;
+	static const int kTextureSize=1024;
+	static const Vec2f kX;
+	static const Vec2f kY;
+
+	int frame_number_;
 };
 
+const Vec2f Homework02App::kX = Vec2f(.5f, -.5f);
+const Vec2f Homework02App::kY = Vec2f(.5f, .5f);
 
 void Homework02App::prepareSettings(Settings* settings){
 	settings->setWindowSize(kAppWidth,kAppHeight);
@@ -40,6 +46,7 @@ void Homework02App::prepareSettings(Settings* settings){
 
 void Homework02App::setup()
 {
+	frame_number_ = 0;
 	mFont = Font("Times New Roman", 28);
 	tempFont = Font("", 0);
 	mTextureFont = gl::TextureFont::create(mFont);
@@ -82,19 +89,40 @@ void Homework02App::mouseDown( MouseEvent event )
 
 void Homework02App::update()
 {
+	Circle* cur = circleList_;
+	Vec2f center = kX * kAppWidth/2.0 + kY * kAppHeight/2.0;
+	 /*
+	if (cur != NULL) {
+		do {
+			cur-> update(center, kAppWidth/2.0);
+			cur = cur->next_;
+		} while (cur != circleList_);
+	} */
+	frame_number_++;
 }
 
 void Homework02App::draw()
 {
 	gl::enableAlphaBlending();
-	gl::clear( Color( 0, 0, 0 ) );
-	
+	gl::clear(Color(0,0,0));
+
+	//Creates the string for instructions
 	std::string str( "Press '?' to toggle these instructions on/off the screen." );
 	Rectf boundsRect( 40, mTextureFont->getAscent() + 40, getWindowWidth() - 40, getWindowHeight() - 40 );
 
-	gl::color( ColorA( 1, 0.5f, 0.25f, 1.0f ) );
-
+	//Text color
+	ColorA semiTransparent = ColorA(.0f,.5f,.0f,.9f);
+	gl::color(semiTransparent);
 	mTextureFont->drawStringWrapped( str, boundsRect );
+
+	Circle* cur = circleList_;
+	/*
+	if(cur != NULL){
+		do {
+			cur->draw(getMousePos());
+			cur = cur->next_;
+		} while (cur != circleList_);
+	} */
 }
 
 CINDER_APP_BASIC( Homework02App, RendererGl )
